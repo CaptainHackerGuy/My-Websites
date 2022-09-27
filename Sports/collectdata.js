17238ce083fb8b903b0b55d6d75e25d1e2375989
CaptainHackerGuy <97624039+CaptainHackerGuy@users.noreply.github.com>
2022-09-27 17:20:39 +0530

    Add files via upload

---
+++ Sports/collectdata.js
@@ -0,0 +1,5 @@
+function CollectData() {
+    var email = document.getElementById('email').value;
+    var name = document.getElementById('name').value;
+    alert('Thank you for contacting us, ' + name + ' We will contact you back through the email ' + email);
+}
