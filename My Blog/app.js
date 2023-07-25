8a1b95b02be1811138667fec9293408918e5f79c
CaptainHackerGuy <97624039+CaptainHackerGuy@users.noreply.github.com>
2023-07-25 20:43:32 +0530

    Delete My Blog directory

--- My Blog/app.js
+++
@@ -1,5 +0,0 @@
-function ContactFormData(){
-    var user_name = document.getElementById('user_name').value;
-    var user_email = document.getElementById('user_email').value;
-    alert('Thank you for contacting us, '+ user_name)
-    }
